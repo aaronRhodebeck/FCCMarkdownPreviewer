3c22629ea856f229d759107ccfd47be649c2e9e8
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-27 15:32:09 -0500

    Finalize build setup, working for Hello World

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export class App extends React.Component {
-    return <h1> Hello World </h1>;
+export class Hello extends React.Component {
+    render() {
+        return <h1> Hello World </h1>;        
+    }
 }
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/app';
+import { Hello } from './components/app';
 
 ReactDOM.render( 
-    <App /> , document.getElementById('app')
+    <Hello /> , document.getElementById('app')
 );
