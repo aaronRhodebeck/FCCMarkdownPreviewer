f7008af69c8110969377a024ee87387688288831
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-27 15:21:37 -0500

    Add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,13 @@ var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 module.exports = {
     entry: __dirname + '/app/index.js',
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
     },
     output: {
         filename: 'transformed.js',
