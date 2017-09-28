20a401701046aa79a7bbc968266f260b56761734
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-28 16:06:46 -0500

    Install marked, imported

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Editor } from '../components/Editor';
 import { PreviewPane } from '../components/PreviewPane';
+import marked from 'marked';
 
 var styles = {
     display: 'flex',
 }
 
 export class App extends React.Component {
+    convertMarkdown() {
+
+    }
     render() {
         return (
             <div style={styles}>
