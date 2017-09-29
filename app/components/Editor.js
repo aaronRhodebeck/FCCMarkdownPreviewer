61fc39e84e44bcba9933a0ac2973f51ca458d69f
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-29 17:25:08 -0500

    Visual improvements

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -3,10 +3,14 @@ import React from 'react';
 var styles = {
     flex: 1,
     background: '#18181B',
-    color: '#726564',
+    color: '#777069',
     padding: 10,
     borderWidth: 3,
     borderColor: '#1F212C',
+    borderStyle: 'solid',
+    resize: 'none',
+    overflowY: 'auto',
+    margin: 0,
 }
 
 // Presentation class of a text editor expecting onChange and onFocus
--- app/components/PreviewPane.js
+++ app/components/PreviewPane.js
@@ -4,6 +4,12 @@ var styles = {
     flex: 1,
     height: '100vh',
     overflow: 'auto',
+    padding: 13,
+    boxSizing: 'border-box',
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    margin: 0
 }
 
 // Presentation class to display passed in HTML requiring text
