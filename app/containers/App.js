007277034882ddffc7020e16b3b8099254a9c376
aaronRhodebeck <arhodebeck@gmail.com>
2017-09-29 15:26:15 -0500

    Add comments to Container/App

--- app/containers/App.js
+++ app/containers/App.js
@@ -6,6 +6,7 @@ import * as Cheatsheet from '../markdownCheatsheet.js';
 import marked from 'marked';
 import highlight from 'highlightjs';
 
+// Create a custom renderer for markdown
 var renderer = new marked.Renderer();
 renderer.link = function (href, title, text) {
     return `<a href="${href}" target="_blank" title="${title}">${text}</a>`
@@ -24,18 +25,19 @@ marked.setOptions({
     }
 })
 
-
+// Styling for the div containing both components of the app
 var styles = {
     display: 'flex',
 }
 
+// Renders the Editor and Previewer
 export class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {editorText: "", htmlToDisplay: {__html: `<h1>Preview</h1>`}};
         this.editorTextChanged = this.editorTextChanged.bind(this);
     }
-
+    
     editorTextChanged(textInEditor) {
         this.setState({editorText: textInEditor});
         this.setState({htmlToDisplay: this.convertToHTML(textInEditor)});
